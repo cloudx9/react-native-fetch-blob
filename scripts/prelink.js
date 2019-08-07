713eb2c9bd51de89316fb8faffa3a3d1458f46e7
cloudx9 <cloudxio9@gmail.com>
2019-08-07 12:51:08 +0200

    fix rnpm

--- scripts/prelink.js
+++ scripts/prelink.js
@@ -11,8 +11,8 @@ try {
 
   console.log('RNFetchBlob detected app version => ' + VERSION);
 
-  if(VERSION < 0.28) {
-    console.log('You project version is '+ VERSION + ' which may not compatible to react-native-fetch-blob 7.0+, please consider upgrade your application template to react-native 0.27+.')
+  if (VERSION < 0.28) {
+    console.log('You project version is ' + VERSION + ' which may not compatible to react-native-fetch-blob 7.0+, please consider upgrade your application template to react-native 0.27+.')
     // add OkHttp3 dependency fo pre 0.28 project
     var main = fs.readFileSync(PACKAGE_GRADLE);
     console.log('adding OkHttp3 dependency to pre 0.28 project .. ')
@@ -23,12 +23,12 @@ try {
 
   console.log('Add Android permissions => ' + (addAndroidPermissions == "true"))
 
-  if(addAndroidPermissions) {
+  if (addAndroidPermissions) {
 
     // set file access permission for Android < 6.0
-    fs.readFile(MANIFEST_PATH, function(err, data) {
+    fs.readFile(MANIFEST_PATH, function (err, data) {
 
-      if(err)
+      if (err)
         console.log('failed to locate AndroidManifest.xml file, you may have to add file access permission manually.');
       else {
 
@@ -52,7 +52,7 @@ try {
   }
   else {
     console.log(
-      '\033[95mreact-native-fetch-blob \033[97mwill not automatically add Android permissions after \033[92m0.9.4 '+
+      '\033[95mreact-native-fetch-blob \033[97mwill not automatically add Android permissions after \033[92m0.9.4 ' +
       '\033[97mplease run the following command if you want to add default permissions :\n\n' +
       '\033[96m\tRNFB_ANDROID_PERMISSIONS=true react-native link \n')
   }
@@ -62,9 +62,9 @@ try {
     return parseFloat(/\d\.\d+(?=\.)/.exec(package.dependencies['react-native']));
   }
 
-} catch(err) {
+} catch (err) {
   console.log(
-    '\033[95mreact-native-fetch-blob\033[97m link \033[91mFAILED \033[97m\nCould not automatically link package :'+
+    '\033[95mreact-native-fetch-blob\033[97m link \033[91mFAILED \033[97m\nCould not automatically link package :' +
     err.stack +
     'please follow the instructions to manually link the library : ' +
     '\033[4mhttps://github.com/wkh237/react-native-fetch-blob/wiki/Manually-Link-Package\n')
