512348035a53d8f22377dcfe5fcf033538347642
cloudx9 <cloudxio9@gmail.com>
2019-08-07 12:50:37 +0200

    fix rnpm

--- react-native.config.js
+++ react-native.config.js
@@ -1,11 +1,8 @@
 
 module.exports = {
-    project: {
-        ios: {},
-        android: {}, // grouped into "project"
+    dependency: {
+        hooks: {
+            prelink: 'node ./node_modules/react-native-fetch-blob/scripts/prelink.js',
+        },
     },
-    assets: ['./path-to-assets'], // stays the same
-    commands: [
-        { "prelink": "node ./node_modules/react-native-fetch-blob/scripts/prelink.js" }
-    ], // formerly "plugin", returns an array of commands
 };
